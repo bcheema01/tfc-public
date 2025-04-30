8df66969780e42da1dde06b02c396b4b2cacf8ae
Sunny Cheema <balwindercheema89@gmail.com>
2025-04-29 23:00:58 -0700

    wordpress

--- main.tf
+++ main.tf
@@ -13,6 +13,8 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
@@ -28,3 +30,102 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
+# Networking and IAM for the EC2 instance
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "web" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "EC2SSMRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "EC2SSMProfile"
+  role = aws_iam_role.ssm_role.name
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+module "wordpress" {
+  source                = "./wordpress-instance"
+  aws_region            = var.aws_region
+  ami_id                = data.aws_ami.amazon_linux.id
+  instance_type         = "t2.micro"
+  subnet_id             = aws_subnet.public.id
+  security_group_id     = aws_security_group.web.id
+  iam_instance_profile  = aws_iam_instance_profile.ssm_profile.name
+  instance_name         = "wordpress-instance"
+}
