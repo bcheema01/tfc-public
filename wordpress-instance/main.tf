1afffe627b5acdb55e4d6fd3e2c8f38105eaca4a
Sunny Cheema <balwindercheema89@gmail.com>
2025-04-29 23:10:15 -0700

    user data

--- wordpress-instance/main.tf
+++ wordpress-instance/main.tf
@@ -79,14 +79,4 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
 
