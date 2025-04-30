eebb9716f8d03fb30705bf4d73c12229475e77c1
Sunny Cheema <balwindercheema89@gmail.com>
2025-04-29 23:03:06 -0700

    wordpress again

--- wordpress-instance/variables.tf
+++ wordpress-instance/variables.tf
@@ -1,3 +1,27 @@
 variable "aws_region" {
-  default = "us-east-1"
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "iam_instance_profile" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
 }
