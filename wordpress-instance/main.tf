58b660be27e66f6379f9b8ddf9a362248bf6e8f6
Sunny Cheema <balwindercheema89@gmail.com>
2025-04-29 23:09:24 -0700

    user data

--- wordpress-instance/main.tf
+++ wordpress-instance/main.tf
@@ -79,9 +79,6 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_iam_role" "ssm_role" {
-  name = "EC2SSMRole"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -92,14 +89,4 @@ resource "aws_iam_role" "ssm_role" {
       Action = "sts:AssumeRole"
     }]
   })
-}
 
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "EC2SSMProfile"
-  role = aws_iam_role.ssm_role.name
-}
